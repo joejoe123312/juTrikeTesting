e434bea8052d987ceefcafa3e9887d7b443b6854
joejoe123312 <joelcenteno123312@gmail.com>
2021-03-03 15:23:12 +0800

    user-service already done

--- src/app/splash-screen/splash-screen.page.ts
+++ src/app/splash-screen/splash-screen.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserServiceService } from '../services/user-service.service';
 
 @Component({
   selector: 'app-splash-screen',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./splash-screen.page.scss'],
 })
 export class SplashScreenPage implements OnInit {
+  userInfo = {
+    userId: null,
+    firstname: null,
+    lastname: null, 
+    phoneNumber: null,
+    fullName: null,
+  };
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private userService: UserServiceService,
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    // get all of the user info and update the values in the user services 
+    await this.userService.getCurrentUserInfo().then(status => {
+      this.router.navigate(['./tabs/home']);
+    }).catch(message => {
+      console.log(message);
+    });
+
+  
+    
   }
 
 }
