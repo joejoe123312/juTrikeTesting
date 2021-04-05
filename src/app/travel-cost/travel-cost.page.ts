4b72c418eca3556527b8a216e8e5967a97407cfe
joejoe123312 <joelcenteno123312@gmail.com>
2021-04-05 10:54:07 +0800

    push kana tangina mo

--- src/app/travel-cost/travel-cost.page.ts
+++ src/app/travel-cost/travel-cost.page.ts
@@ -102,7 +102,7 @@ export class TravelCostPage implements OnInit {
         // calculate the distance and duration
         await this.mapsService.getDurationAndDistance(startLat, startLng, endLat, endLng).then(returnValue => {
 
-          let firstTravelTimeAndDistance = {
+          let secondTravelTimeAndDistance = {
             startLat:startLat,
             startLng:startLng,
             endLat:endLat,
@@ -110,13 +110,13 @@ export class TravelCostPage implements OnInit {
             distanceAndDuration: returnValue,
           };
 
-          this.secondTravelTimeAndDistance = firstTravelTimeAndDistance;
+          this.secondTravelTimeAndDistance = secondTravelTimeAndDistance;
 
           let secondSummaryObj = {
             pickUp: pickUpLocation.location,
             dropOff: secondDropOffLocation.location,
-            distance: this.firstTravelTimeAndDistance.distanceAndDuration.distance,
-            duration: this.firstTravelTimeAndDistance.distanceAndDuration.duration,
+            distance: this.secondTravelTimeAndDistance.distanceAndDuration.distance,
+            duration: this.secondTravelTimeAndDistance.distanceAndDuration.duration,
           };
 
           this.secondSummary = secondSummaryObj;
@@ -169,36 +169,45 @@ export class TravelCostPage implements OnInit {
   }
 
 
-  proceed(){
+  async proceed(){
 
+    // initialize current user info
+    await this.userService.getCurrentUserInfo();
+    let secondPassengerName = this.mapsService.getSecondPassengerName();
+    let objectSet = {
+      "createdAt": Date.now(),
+      "quantity_of_commuters": this.commuterSelector, // will vary
+      "first_summary": {
+        startLat: this.firstTravelTimeAndDistance.startLat,
+        startLng: this.firstTravelTimeAndDistance.startLng,
+        endLat: this.firstTravelTimeAndDistance.endLng,
+        endLng: this.firstTravelTimeAndDistance.endLng,
+        distance: this.firstSummary.distance,
+        duration: this.firstSummary.duration,
+        price: 100,
+      },
+      "second_summary": null, // will change later if commuterSelector is 2 and sameDropOffLocation = false
+      "second_passenger_name": secondPassengerName,
+      "personal_information_passenger1": this.userService.getUserInfo(),
+      "status": 'waiting',
+    }
 
-    // const startLocation = pickUpLoc.location;
-    // const startLat = pickUpLoc.latitude;
-    // const startLong = pickUpLoc.longitude;
+    // determine if there is need for the second_summary
+    if (this.sameDropOffLocation == false) {
+      objectSet.second_summary = {
+        startLat: this.secondTravelTimeAndDistance.startLat,
+        startLng: this.secondTravelTimeAndDistance.startLng,
+        endLat: this.secondTravelTimeAndDistance.endLng,
+        endLng: this.secondTravelTimeAndDistance.endLng,
+        distance: this.secondSummary.distance,
+        duration: this.secondSummary.duration,
+        price: 100,
+      }
+    }
 
-    // const endLocation = dropOffLoc.location;
-    // const endLat = dropOffLoc.latitude;
-    // const endLong = dropOffLoc.longitude;
-
-    // // configure object
-    // let objectSet = {
-    //   "createdAt": Date.now(),
-    //   "start_location": startLocation,
-    //   "start_latitude": startLat,
-    //   "start_longitude": startLong,
-    //   "end_location": endLocation,
-    //   "end_latitude": endLat,
-    //   "end_longitude": endLong,
-    //   "distance": this.distance,
-    //   "price": "100",
-    //   "status": "finding",
-
-    //   // personal information
-    //   // "fullName": this.fullName,
-    //   "mobile_number": "Hehe",
-    // }
+    console.log(objectSet);
 
     // // create field in the db
-    // this.crudService.create('bookings', objectSet, '/driver-otw');
+    this.crudService.create('bookings', objectSet, '/driver-otw');
   }
 }
